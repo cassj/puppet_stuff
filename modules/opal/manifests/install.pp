c39a887a53b1f66641801e510cb80f01080fb993
Cass Johnston <cassjohnston@gmail.com>
2012-09-12 13:23:29 +0100

    updated opal to use obiba debian repository

--- modules/opal/manifests/install.pp
+++ modules/opal/manifests/install.pp
@@ -3,27 +3,17 @@ class opal::install {
   case $operatingsystem{
     /Ubuntu|Debian/:{
 
-      # They have a repository now, do:
-      # wget -q -O - http://pkg.obiba.org/obiba.org.key | sudo apt-key add -
-      # and in /etc/apt/sources.list: deb http://pkg.obiba.org stable/
-     
-
-      file{"/opt/debs":
-        ensure => directory
+      exec {"opal-obiba-key":
+        command => "/usr/bin/wget http://pkg.obiba.org/obiba.org.key && apt-key add obiba.org.key ", 
+        unless  => "/bin/grep 'obiba' /etc/apt/sources.list"
+      }
+      exec{"opal-obiba-repo":
+         command => '/bin/echo "\ndeb http://pkg.obiba.org stable/ \n" /etc/apt/sources.list',
+         unless  => "/bin/grep 'obiba' /etc/apt/sources.list "
       }
 
-      file{"/opt/debs/opal.deb":
-        ensure => present,
-        owner  => root,
-        group  => root,
-        mode   => 644,
-        source => "puppet:///modules/opal/opal.deb"
-      }
-  
       package { "opal":
-       provider => dpkg,
        ensure   => latest,
-       source   => "/opt/debs/opal..deb"
       }
     }
     /(RedHat|CentOS|Fedora|Scientific)/:{
